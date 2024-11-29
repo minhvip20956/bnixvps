4ce8ea6750a6e19a716339121614e6f100197745
Cong Minh Vuong <cminh.v566@gmail.com>
2024-11-29 20:18:34 +0700

    Update DiskExtension.bat

--- DiskExtension.bat
+++ DiskExtension.bat
@@ -1,4 +1,12 @@
 @echo off
+cd C:\Windows\Setup\
+mkdir Scripts
+cd C:\Windows\Setup\Scripts\
+echo "list disk" > diskpart.txt
+echo "select disk 0" >> diskpart.txt
+echo "list partition" >> diskpart.txt
+echo "select partition 2" >> diskpart.txt
+echo "extend" >> diskpart.txt
 diskpart /s C:\Windows\Setup\Scripts\diskpart.txt
 del /Q /F C:\Windows\System32\Sysprep\unattend.xml
 del /Q /F C:\Windows\Panther\unattend.xml
